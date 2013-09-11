b2809a011075366bb9c355cd3766b1ba6bc9aa38
Youngjae Shin <yj99.shin@samsung.com>
2013-09-11 17:26:22 +0900

    apply new smartcard service(0.1.27)
    Change-Id: I1cbb606394606ca68da38f8f19a2f73a1975973a

--- UICCTerminal.cpp
+++ UICCTerminal.cpp
@@ -15,14 +15,15 @@
  */
 
 /* standard library header */
-#include <stdio.h>
+#include <cstdio>
+#include <cstring>
 #include <unistd.h>
-#include <string.h>
 #include <sys/time.h>
 
 /* SLP library header */
 
 /* local header */
+#include "smartcard-types.h"
 #include "Debug.h"
 #include "TerminalInterface.h"
 #include "UICCTerminal.h"
@@ -51,7 +52,7 @@ void __attribute__ ((destructor)) lib_fini()
 {
 }
 
-/* below trhee functions must be implemented */
+/* below three functions must be implemented */
 extern "C" EXPORT_API const char *get_name()
 {
 	return se_name;
@@ -71,87 +72,83 @@ extern "C" EXPORT_API void destroy_instance(void *instance)
 	}
 	else
 	{
-		SCARD_DEBUG_ERR("instance is invalid : getInstance [%p], instance [%p]", UICCTerminal::getInstance(), instance);
+		_ERR("instance is invalid : getInstance [%p], instance [%p]",
+			UICCTerminal::getInstance(), instance);
 	}
 }
 
-static void _uiccTransmitCallback(TapiHandle *handle, int result, void *data, void *user_data)
+static void _uiccTransmitCallback(TapiHandle *handle, int result,
+	void *data, void *user_data)
 {
 	TelSimAccessResult_t access_rt = (TelSimAccessResult_t)result;
 	TelSimApduResp_t *apdu = (TelSimApduResp_t *)data;
 	callback_param_t *param = (callback_param_t *)user_data;
 
-	SCARD_DEBUG("APDU response");
-
 	if (param != NULL)
 	{
 		if (param->callback != NULL)
 		{
-			terminalTransmitCallback callback = (terminalTransmitCallback)param->callback;
+			terminalTransmitCallback callback =
+				(terminalTransmitCallback)param->callback;
 
 			if (apdu != NULL && apdu->apdu_resp_len > 0)
 			{
-				callback(apdu->apdu_resp, apdu->apdu_resp_len, access_rt, param->param);
+				callback(apdu->apdu_resp, apdu->apdu_resp_len,
+					access_rt, param->param);
 			}
 			else
 			{
 				callback(NULL, 0, access_rt, param->param);
 			}
 		}
-		else
-		{
-			SCARD_DEBUG("there is no callback");
-		}
 
 		delete param;
 	}
 	else
 	{
-		SCARD_DEBUG_ERR("invalid param");
+		_ERR("invalid param");
 	}
 }
 
-static void _uiccGetATRCallback(TapiHandle *handle, int result, void *data, void *user_data)
+static void _uiccGetATRCallback(TapiHandle *handle, int result,
+	void *data, void *user_data)
 {
 	TelSimAccessResult_t access_rt = (TelSimAccessResult_t)result;
 	TelSimAtrResp_t *atr  = (TelSimAtrResp_t *)data;
 	callback_param_t *param = (callback_param_t *)user_data;
 
-	SCARD_DEBUG("APDU response");
-
 	if (param != NULL)
 	{
 		if (param->callback != NULL)
 		{
-			terminalGetATRCallback callback = (terminalGetATRCallback)param->callback;
+			terminalGetATRCallback callback =
+				(terminalGetATRCallback)param->callback;
 
 			if (atr != NULL && atr->atr_resp_len > 0)
 			{
-				callback(atr->atr_resp, atr->atr_resp_len, access_rt, param->param);
+				callback(atr->atr_resp, atr->atr_resp_len,
+					access_rt, param->param);
 			}
 			else
 			{
 				callback(NULL, 0, access_rt, param->param);
 			}
 		}
-		else
-		{
-			SCARD_DEBUG("there is no callback");
-		}
 
 		delete param;
 	}
 	else
 	{
-		SCARD_DEBUG_ERR("invalid param");
+		_ERR("invalid param");
 	}
 }
 
 namespace smartcard_service_api
 {
-	UICCTerminal::UICCTerminal()
+	UICCTerminal::UICCTerminal() : Terminal(), complete(false)
 	{
 		name = (char *)se_name;
+
 		initialize();
 	}
 
@@ -169,37 +166,36 @@ namespace smartcard_service_api
 
 	bool UICCTerminal::initialize()
 	{
-		SCARD_BEGIN();
+		_BEGIN();
 
 		if (initialized == false)
 		{
-			char **cpList = NULL;
-
-//			cpList = tel_get_cp_name_list();
-
 			handle = tel_init(NULL);
 			if (handle != NULL)
 			{
 				int error;
 
-				error = tel_register_noti_event(handle, TAPI_NOTI_SIM_STATUS, &UICCTerminal::uiccStatusNotiCallback, this);
+				error = tel_register_noti_event(handle,
+					TAPI_NOTI_SIM_STATUS,
+					&UICCTerminal::uiccStatusNotiCallback,
+					this);
 
 				initialized = true;
 			}
 			else
 			{
-				SCARD_DEBUG_ERR("tel_init failed");
+				_ERR("tel_init failed");
 			}
 		}
 
-		SCARD_END();
+		_END();
 
 		return initialized;
 	}
 
 	void UICCTerminal::finalize()
 	{
-		SCARD_BEGIN();
+		_BEGIN();
 
 		if (isInitialized())
 		{
@@ -210,162 +206,165 @@ namespace smartcard_service_api
 			initialized = false;
 		}
 
-		SCARD_END();
+		_END();
 	}
 
-	int UICCTerminal::transmitSync(ByteArray command, ByteArray &response)
+	int UICCTerminal::transmitSync(const ByteArray &command,
+		ByteArray &response)
 	{
-		int result = -1;
+		int result;
 
-		SCARD_BEGIN();
+		_BEGIN();
 
 		SCOPE_LOCK(mutex)
 		{
-			if (command.getLength() > 0)
+			if (command.size() > 0)
 			{
 				TelSimApdu_t apdu_data = { 0, };
 
-				apdu_data.apdu = command.getBuffer();
-				apdu_data.apdu_len = command.getLength();
+				apdu_data.apdu = (uint8_t *)command.getBuffer();
+				apdu_data.apdu_len = command.size();
 
-				syncLock();
+				complete = false;
+				error = 0;
+				this->response.clear();
 
-				result = tel_req_sim_apdu(handle, &apdu_data, &UICCTerminal::uiccTransmitAPDUCallback, this);
+				result = tel_req_sim_apdu(handle, &apdu_data,
+					&UICCTerminal::uiccTransmitAPDUCallback,
+					this);
 				if (result == 0)
 				{
-					SCARD_DEBUG("tel_req_sim_apdu request is success");
+					while (complete == false) {
+						g_main_context_iteration(NULL,
+							false);
+					}
 
-					error = 0;
-					this->response.releaseBuffer();
+					result = error;
 
-					result = waitTimedCondition(3);
-
-
-					if (result == 0 && error == 0)
-					{
-						if (this->response.getLength() > 0)
+					if (error == SCARD_ERROR_OK &&
+						this->response.size() > 0)
 						{
 							response = this->response;
-						}
-
-						SCARD_DEBUG("tel_req_sim_apdu success, length [%d]", response.getLength());
 					}
 					else
 					{
-						SCARD_DEBUG_ERR("tel_req_sim_apdu failed, result [%d], cbResult [%d]", result, error);
+						_ERR("tel_req_sim_apdu failed, cbResult [%d]", error);
 					}
 				}
 				else
 				{
-					SCARD_DEBUG_ERR("tel_req_sim_apdu failed [%d]", result);
+					_ERR("tel_req_sim_apdu failed [%d]", result);
+					result = SCARD_ERROR_IO_FAILED;
 				}
-
-				syncUnlock();
 			}
 			else
 			{
-				SCARD_DEBUG_ERR("apdu is empty");
+				_ERR("apdu is empty");
+				result = SCARD_ERROR_ILLEGAL_PARAM;
 			}
 		}
 
-		SCARD_END();
+		_END();
 
 		return result;
 	}
 
 	int UICCTerminal::getATRSync(ByteArray &atr)
 	{
-		int result = 0;
+		int result;
 
-		SCARD_BEGIN();
+		_BEGIN();
 
 		SCOPE_LOCK(mutex)
 		{
-			syncLock();
+			complete = false;
+			error = 0;
+			this->response.clear();
 
-			result = tel_req_sim_atr(handle, &UICCTerminal::uiccGetAtrCallback, this);
+			result = tel_req_sim_atr(handle,
+				&UICCTerminal::uiccGetAtrCallback, this);
 			if (result == 0)
 			{
-				SCARD_DEBUG("tel_req_sim_atr request is success");
+				while (complete == false) {
+					g_main_context_iteration(NULL,
+						false);
+				}
 
-				error = 0;
-				this->response.releaseBuffer();
+				result = error;
 
-				result = waitTimedCondition(3);
-
-				if (result == 0 && error == 0)
-				{
-					if (this->response.getLength() > 0)
+				if (error == SCARD_ERROR_OK &&
+					this->response.size() > 0)
 					{
 						atr = this->response;
-					}
-
-					SCARD_DEBUG("tel_req_sim_atr success, length [%d]", response.getLength());
 				}
 				else
 				{
-					SCARD_DEBUG_ERR("tel_req_sim_atr failed, result [%d], cbResult [%d]", result, error);
+					_ERR("tel_req_sim_atr failed, cbResult [%d]", error);
 				}
 			}
 			else
 			{
-				SCARD_DEBUG_ERR("tel_req_sim_atr failed [%d]", result);
+				_ERR("tel_req_sim_atr failed [%d]", result);
+				result = SCARD_ERROR_IO_FAILED;
 			}
-
-			syncUnlock();
 		}
 
-		SCARD_END();
+		_END();
 
 		return result;
 	}
 
-	int UICCTerminal::transmit(ByteArray command, terminalTransmitCallback callback, void *userParam)
+	int UICCTerminal::transmit(const ByteArray &command,
+		terminalTransmitCallback callback, void *userParam)
 	{
-		int result = -1;
+		int result;
 
-		SCARD_BEGIN();
+		_BEGIN();
 
 		SCOPE_LOCK(mutex)
 		{
-			if (command.getLength() > 0)
+			if (command.size() > 0)
 			{
 				TelSimApdu_t apdu_data = { 0, };
 				callback_param_t *param = NULL;
 
-				apdu_data.apdu = command.getBuffer();
-				apdu_data.apdu_len = command.getLength();
+				apdu_data.apdu = (uint8_t *)command.getBuffer();
+				apdu_data.apdu_len = command.size();
 
 				param = new callback_param_t();
 				param->callback = (void *)callback;
 				param->param = userParam;
 
-				result = tel_req_sim_apdu(handle, &apdu_data, _uiccTransmitCallback, param);
+				result = tel_req_sim_apdu(handle, &apdu_data,
+					_uiccTransmitCallback, param);
 				if (result == 0)
 				{
-					SCARD_DEBUG("tel_req_sim_apdu request is success");
+					_DBG("tel_req_sim_apdu request is success");
 				}
 				else
 				{
-					SCARD_DEBUG_ERR("tel_req_sim_apdu failed [%d]", result);
+					_ERR("tel_req_sim_apdu failed [%d]", result);
+					result = SCARD_ERROR_IO_FAILED;
 				}
 			}
 			else
 			{
-				SCARD_DEBUG_ERR("apdu is empty");
+				_ERR("apdu is empty");
+				result = SCARD_ERROR_ILLEGAL_PARAM;
 			}
 		}
 
-		SCARD_END();
+		_END();
 
 		return result;
 	}
 
-	int UICCTerminal::getATR(terminalGetATRCallback callback, void *userParam)
+	int UICCTerminal::getATR(terminalGetATRCallback callback,
+		void *userParam)
 	{
-		int result = 0;
+		int result;
 
-		SCARD_BEGIN();
+		_BEGIN();
 
 		SCOPE_LOCK(mutex)
 		{
@@ -375,67 +374,61 @@ namespace smartcard_service_api
 			param->callback = (void *)callback;
 			param->param = userParam;
 
-			result = tel_req_sim_atr(handle, _uiccGetATRCallback, param);
+			result = tel_req_sim_atr(handle,
+				_uiccGetATRCallback, param);
 			if (result == 0)
 			{
-				SCARD_DEBUG("tel_req_sim_atr request is success");
+				_DBG("tel_req_sim_atr request is success");
 			}
 			else
 			{
-				SCARD_DEBUG_ERR("tel_req_sim_atr failed [%d]", result);
+				_ERR("tel_req_sim_atr failed [%d]", result);
+				result = SCARD_ERROR_IO_FAILED;
 			}
 		}
 
-		SCARD_END();
+		_END();
 
 		return result;
 	}
 
-	bool UICCTerminal::isSecureElementPresence()
+	bool UICCTerminal::isSecureElementPresence() const
 	{
 		bool result = false;
 		int error = 0;
 		TelSimCardStatus_t state = (TelSimCardStatus_t)0;
 		int cardChanged = 0;
 
-		SCARD_BEGIN();
+		_BEGIN();
 
 		error = tel_get_sim_init_info(handle, &state, &cardChanged);
-
-		SCARD_DEBUG("current sim init state = [%d], error [%d], cardChanged [%d]", state, error, cardChanged);
-
 		if (error == 0)
 		{
-			if (state == TAPI_SIM_STATUS_SIM_INIT_COMPLETED || state == TAPI_SIM_STATUS_SIM_INITIALIZING)
+			if (state == TAPI_SIM_STATUS_SIM_INIT_COMPLETED)
 			{
-				SCARD_DEBUG("sim is initialized");
-
 				result = true;
 			}
 			else
 			{
-				SCARD_DEBUG_ERR("sim is not initialized");
+				_ERR("sim is not initialized, state [%d]", state);
 			}
 		}
 		else
 		{
-			SCARD_DEBUG_ERR("error = [%d]", error);
+			_ERR("tel_get_sim_init_info failed, [%d]", error);
 		}
 
-		SCARD_END();
+		_END();
 
 		return result;
 	}
 
-	void UICCTerminal::uiccTransmitAPDUCallback(TapiHandle *handle, int result, void *data, void *user_data)
+	void UICCTerminal::uiccTransmitAPDUCallback(TapiHandle *handle,
+		int result, void *data, void *user_data)
 	{
 		UICCTerminal *instance = (UICCTerminal *)user_data;
 		TelSimAccessResult_t access_rt = (TelSimAccessResult_t)result;
 		TelSimApduResp_t *apdu = (TelSimApduResp_t *)data;
-
-		SCARD_DEBUG("APDU response");
-
-		instance->syncLock();
 
 		instance->error = access_rt;
 
@@ -443,79 +436,79 @@ namespace smartcard_service_api
 		{
 			if (apdu != NULL && apdu->apdu_resp_len > 0)
 			{
-				instance->response.setBuffer(apdu->apdu_resp, apdu->apdu_resp_len);
+				instance->response.assign(apdu->apdu_resp,
+					apdu->apdu_resp_len);
 			}
 
-			SCARD_DEBUG("response : %s", instance->response.toString());
+			_DBG("response : %s", instance->response.toString().c_str());
+			instance->error = SCARD_ERROR_OK;
 		}
 		else
 		{
-			SCARD_DEBUG_ERR("error : event->Status == [%d]", access_rt);
+			_ERR("error : event->Status == [%d]", access_rt);
+			instance->error = SCARD_ERROR_IO_FAILED;
 		}
 
-		instance->signalCondition();
-		instance->syncUnlock();
+		instance->complete = true;
 	}
 
-	void UICCTerminal::uiccGetAtrCallback(TapiHandle *handle, int result, void *data, void *user_data)
+	void UICCTerminal::uiccGetAtrCallback(TapiHandle *handle, int result,
+		void *data, void *user_data)
 	{
 		UICCTerminal *instance = (UICCTerminal *)user_data;
 		TelSimAccessResult_t access_rt = (TelSimAccessResult_t)result;
 		TelSimAtrResp_t *atr  = (TelSimAtrResp_t *)data;
 
-		SCARD_DEBUG("Get ATR response");
-
-		instance->syncLock();
-
-		instance->error = access_rt;
-
 		if (access_rt == 0)
 		{
 			if (atr != NULL && atr->atr_resp_len > 0)
 			{
-				instance->response.setBuffer(atr->atr_resp, atr->atr_resp_len);
+				instance->response.assign(atr->atr_resp,
+					atr->atr_resp_len);
 			}
 
-			SCARD_DEBUG("response : %s", instance->response.toString());
+			_DBG("response : %s", instance->response.toString().c_str());
+			instance->error = SCARD_ERROR_OK;
 		}
 		else
 		{
-			SCARD_DEBUG_ERR("error : event->Status == [%d]", access_rt);
+			_ERR("error : event->Status == [%d]", access_rt);
+			instance->error = SCARD_ERROR_IO_FAILED;
 		}
 
-		instance->signalCondition();
-		instance->syncUnlock();
+		instance->complete = true;
 	}
 
-	void UICCTerminal::uiccStatusNotiCallback(TapiHandle *handle, const char *noti_id, void *data, void *user_data)
+	void UICCTerminal::uiccStatusNotiCallback(TapiHandle *handle,
+		const char *noti_id, void *data, void *user_data)
 	{
 		UICCTerminal *instance = (UICCTerminal *)user_data;
 		TelSimCardStatus_t *status = (TelSimCardStatus_t *)data;
 
-		SCARD_DEBUG("TAPI_NOTI_SIM_STATUS");
-
 		switch (*status)
 		{
 		case TAPI_SIM_STATUS_SIM_INIT_COMPLETED :
-			SCARD_DEBUG("TAPI_SIM_STATUS_SIM_INIT_COMPLETED");
+			_DBG("TAPI_SIM_STATUS_SIM_INIT_COMPLETED");
 
 			if (instance->statusCallback != NULL)
 			{
-				instance->statusCallback((void *)se_name, NOTIFY_SE_AVAILABLE, 0, NULL);
+				instance->statusCallback((void *)se_name,
+					NOTIFY_SE_AVAILABLE, 0, NULL);
 			}
 			break;
 
 		case TAPI_SIM_STATUS_CARD_REMOVED :
-			SCARD_DEBUG("TAPI_SIM_STATUS_CARD_REMOVED");
+			_DBG("TAPI_SIM_STATUS_CARD_REMOVED");
 
 			if (instance->statusCallback != NULL)
 			{
-				instance->statusCallback((void *)se_name, NOTIFY_SE_NOT_AVAILABLE, 0, NULL);
+				instance->statusCallback((void *)se_name,
+					NOTIFY_SE_NOT_AVAILABLE, 0, NULL);
 			}
 			break;
 
 		default :
-			SCARD_DEBUG("unknown status [%d]", *status);
+			/* ignore notification */
 			break;
 		}
 	}
--- packaging/smartcard-plugin-uicc.spec
+++ packaging/smartcard-plugin-uicc.spec
@@ -1,20 +1,20 @@
 Name:       smartcard-plugin-uicc
 Summary:    Smartcard plugin uicc
-Version:    0.0.3
+Version:          0.0.7
 Release:    0
-Group:      libs
-License:    Apache License, Version 2.0
+Group:      Telephony/Service
+License:    Apache-2.0
 Source0:    %{name}-%{version}.tar.gz
 Source1001: 	smartcard-plugin-uicc.manifest
+#ExclusiveArch:    %%arm
+BuildRequires:    cmake
+BuildRequires:    pkgconfig(glib-2.0)
 BuildRequires: pkgconfig(dlog)
 BuildRequires: pkgconfig(tapi)
-BuildRequires: pkgconfig(smartcard-service)
 BuildRequires: pkgconfig(smartcard-service-common)
-BuildRequires: cmake
-BuildRequires: gettext-tools
 Requires(post):   /sbin/ldconfig
-Requires(post):   /usr/bin/vconftool
-requires(postun): /sbin/ldconfig
+Requires(postun): /sbin/ldconfig
+
 
 %description
 Smartcard Service plugin uicc
@@ -23,41 +23,18 @@ Smartcard Service plugin uicc
 %setup -q
 cp %{SOURCE1001} .
 
-%package    devel
-Summary:    smartcard service uicc
-Group:      Development/Libraries
-Requires:   %{name} = %{version}-%{release}
-
-%description devel
-smartcard service.
-
 %build
-mkdir obj-arm-limux-qnueabi
-cd obj-arm-limux-qnueabi
-cmake .. -DCMAKE_INSTALL_PREFIX=%{_prefix}
-#make %{?jobs:-j%jobs}
+%cmake .
 
 %install
-cd obj-arm-limux-qnueabi
 %make_install
-mkdir -p %{buildroot}/usr/share/license
-cp -af %{_builddir}/%{name}-%{version}/packaging/smartcard-plugin-uicc %{buildroot}/usr/share/license/
 
-%post
-/sbin/ldconfig
+%post -p /sbin/ldconfig
 
-
-%postun
-/sbin/ldconfig
-
-#%post
-# -n nfc-common-lib -p /sbin/ldconfig
-
-#%postun
-# -n nfc-common-lib -p /sbin/ldconfig
+%postun -p /sbin/ldconfig
 
 %files
 %manifest %{name}.manifest
 %defattr(-,root,root,-)
-/usr/lib/se/lib*.so
-/usr/share/license/smartcard-plugin-uicc
+%{_libdir}/se/lib*.so
+%license LICENSE.APLv2
